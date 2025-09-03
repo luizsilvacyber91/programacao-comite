b2f8fffaebef9714696017c32f1cf2c9d843b611
luizsilvacyber91 <didi.luizhenrique@yahoo.com.br>
2025-09-02 21:54:28 -0300

    criação doprograma de tarefas

---
+++ tarefas.py
@@ -0,0 +1,14 @@
+
+numero_1 = int (input("informe um numero inteiro ->"))
+print("o dobro do numero informado è ->", numero_1*2)
+
+#calculando a area do quadrado
+lado = float( input(" informe o valor do quadrado -> "))
+print(" a area do quadrado é-> ", lado * lado)
+idade = int (input("quantos anos vc tem -> "))
+print ("a sua idade è ",idade ,"em meses são", idade*12, "e" , idade*365 ,"dias" )
+nota1 = float(input("informe a primeira nota "))
+nota2 = float(input("informe a segunda nota "))
+nota3 = float(input("informe a terceira nota "))
+
+print("a media é ->",(nota1 + nota2 + nota3) / 3)

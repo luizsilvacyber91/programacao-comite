4e551c5245f6f396170f4328759ad19a4ee265e3
luizsilvacyber91 <didi.luizhenrique@yahoo.com.br>
2025-09-03 21:49:56 -0300

    inicio do conteudo de condicionais

---
+++ condicionais/par_impar.py
@@ -0,0 +1,8 @@
+numero = int(input("informe um numero ->"))
+
+if(numero % 2) == 0:
+
+
+    print("0 numero è par")
+else:
+    print("0 numero è impar")
---
+++ condicionais/verificar_positivo.py
@@ -0,0 +1,9 @@
+numero = int(input("informe um numero ->"))
+
+if numero > 0:
+    print("0 numero è positivo")
+elif numero < 0: 
+    print("0 numero é negativo")
+else:
+    print("0 numero não è positivo") 
+    
